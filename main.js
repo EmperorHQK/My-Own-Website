b824c6fa3961a7500f61abeb308deeef62c4c2b9
offic <officialzhander@gmail.com git config --global user.name offic>
2022-04-04 18:36:09 -0700

    FirstCommit

---
+++ main.js
@@ -0,0 +1,8 @@
+
+emailE='gmail.com'
+emailE=('offigstudentwebsite' + '@' + emailE)
+document.write('<A href="mailto:' + emailE + '">' + emailE + '</a>')
+
+const div = document.getElementById("Email");
+
+div.style.color = "white";
